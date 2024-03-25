8899b12078b84310e2190f973493e13f463e163c
CuteZhuk <s22349@pjwstk.edu.pl>
2024-03-25 12:43:37 +0100

    git add

--- obj/Debug/net8.0/APBD_Cw2.AssemblyInfo.cs
+++ obj/Debug/net8.0/APBD_Cw2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD_Cw2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba1878bc5d407804a28a46c37eb524cdf5894a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78bab1e13d8e52816a3077681a6c5b7b91851a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD_Cw2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD_Cw2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
